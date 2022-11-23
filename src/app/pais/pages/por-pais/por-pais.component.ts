7bed60a6c3736e936ebc79d66c6f76a71ee5fd7d
pacha Bauer <eabestudio@gmail.com>
2022-11-23 14:23:35 -0300

    naty

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styleUrls: ['./por-pais.component.css'],
+})
+export class PorPaisComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) {}
+
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+    this.paisService.buscarPais(this.termino).subscribe({
+      next: (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      error: (err) => {
+        this.hayError = true;
+        this.paises = [];
+      },
+    });
+  }
+}
